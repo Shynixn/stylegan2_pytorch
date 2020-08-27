439b4d4ab5a7dba058c4f383d642bf7c6f0fa6f3
Christoph <shynixn@gmail.com>
2020-08-27 18:03:42 +0200

    Changed to CPU.

--- stylegan2/external_models/lpips.py
+++ stylegan2/external_models/lpips.py
@@ -45,7 +45,7 @@ class LPIPS_VGG16(nn.Module):
             idx_range = range(self._FEATURE_IDX[i - 1], self._FEATURE_IDX[i])
             self.slices.append(nn.Sequential(*[features[j] for j in idx_range]))
         self.linear_layers = nn.ModuleList()
-        for weight in torch.utils.model_zoo.load_url(self._LINEAR_WEIGHTS_URL).values():
+        for weight in torch.utils.model_zoo.load_url(self._LINEAR_WEIGHTS_URL, map_location=torch.device('cpu')).values():
             weight = weight.view(1, -1)
             linear = nn.Linear(weight.size(1), 1, bias=False)
             linear.weight.data.copy_(weight)
